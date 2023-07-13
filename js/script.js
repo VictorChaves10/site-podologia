b75a3ee1ed3d51eb33467c4b0f4def6a8b0c5cab
Victor Chaves <victor.chaves460@gmail.com>
2023-07-13 19:10:28 -0300

    create responsive menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
- 
+window.addEventListener('resize', function () {
+  var screenWidth = window.innerWidth || document.documentElement.clientWidth;
+  var menuItems = document.querySelector('.itens-menu');
+  var menuButton = document.querySelector('.navbar-toggler');
+
+  if (screenWidth < 760) {
+      menuItems.classList.add('d-none');
+      menuButton.classList.remove('d-none');
+  } else {
+      menuItems.classList.remove('d-none');
+      menuButton.classList.add('d-none');
+  }
+});
+
+// Disparar o evento resize ao carregar a página
+window.dispatchEvent(new Event('resize'));
+
+
+
+
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 25,
