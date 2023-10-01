b79c3bde81f3b21e35171e8768e37792b81cff4f
Victor Chaves <victor.chaves460@gmail.com>
2023-10-01 11:58:44 -0300

    I deleted the swipper library

--- js/script.js
+++ js/script.js
@@ -14,36 +14,3 @@ window.addEventListener('resize', function () {
 
 // Disparar o evento resize ao carregar a página
 window.dispatchEvent(new Event('resize'));
-
-
-
-
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-    },
-  });
